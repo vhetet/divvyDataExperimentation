8d73022b73f7214a672a2823c621df3e533f446d
vhetet <vh1030478@gmail.com>
2020-08-19 19:41:30 -0500

    add graph container component

---
+++ src/components/GraphContainer.vue
@@ -0,0 +1,34 @@
+<template>
+    <h1>Container that fetch and distribute data to the graphs</h1>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'GraphContainer',
+  props: {
+  },
+  created() {
+    // fetch the data when the view is created and the data is
+    // already being observed
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.error = this.post = null;
+      this.loading = true;
+      console.log("tst");
+      axios
+        .get(
+          "https://data.cityofchicago.org/resource/fg6s-gzvg.json?from_station_id=418"
+        )
+        .then(response => {
+            console.log(response.data)
+        })
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
