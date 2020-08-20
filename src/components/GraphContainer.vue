4c4d38d8515b5ec7c254af0d84b070662c5c6d3a
vhetet <vh1030478@gmail.com>
2020-08-19 22:37:36 -0500

    fetch data and show average time of a trip per user type

--- src/components/GraphContainer.vue
+++ src/components/GraphContainer.vue
@@ -1,34 +1,78 @@
 <template>
-    <h1>Container that fetch and distribute data to the graphs</h1>
+    <div>
+        <h1>Average time or trip per user type</h1>
+        <div v-for="key in averageTimePerCustomerTypeKeys" :key="key">
+            <h4>Type: {{ key }}</h4>
+            <p>Number of trips{{ averageTimePerCustomerType[key].totalTrip }}</p>
+            <p>{{ averageTimePerCustomerType[key].value | formatToMinutes }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import moment from "moment";
 
 export default {
-  name: 'GraphContainer',
-  props: {
-  },
-  created() {
-    // fetch the data when the view is created and the data is
-    // already being observed
-    this.fetchData();
-  },
-  methods: {
-    fetchData() {
-      this.error = this.post = null;
-      this.loading = true;
-      console.log("tst");
-      axios
-        .get(
-          "https://data.cityofchicago.org/resource/fg6s-gzvg.json?from_station_id=418"
-        )
-        .then(response => {
-            console.log(response.data)
-        })
+    name: "GraphContainer",
+    data: function() {
+        return {
+            data: [],
+            averageTimePerCustomerType: []
+        };
+    },
+    computed: {
+        averageTimePerCustomerTypeKeys: function() {
+            return Object.keys(this.averageTimePerCustomerType);
+        }
+    },
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            this.error = this.post = null;
+            this.loading = true;
+            console.log("tst");
+            axios
+                .get(
+                    "https://data.cityofchicago.org/resource/fg6s-gzvg.json?from_station_id=418"
+                )
+                .then(response => {
+                    console.log(response.data[0]);
+                    this.data = response.data;
+                    this.getAverageTimePerCustomerType();
+                });
+        },
+        getAverageTimePerCustomerType() {
+            let average = {};
+            this.data.forEach(trip => {
+                let diff = moment(trip.stop_time).diff(moment(trip.start_time));
+                if (!average[trip.user_type]) {
+                    average[trip.user_type] = {
+                        totalTime: diff,
+                        totalTrip: 1,
+                        value: diff
+                    };
+                } else {
+                    average[trip.user_type].totalTime += diff;
+                    average[trip.user_type].totalTrip++;
+                    average[trip.user_type].value =
+                        average[trip.user_type].totalTime /
+                        average[trip.user_type].totalTrip;
+                }
+            });
+            this.averageTimePerCustomerType = average;
+        }
+    },
+    filters: {
+        formatToMinutes(millis) {
+            var minutes = Math.floor(millis / 60000);
+            var seconds = ((millis % 60000) / 1000).toFixed(0);
+            return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+        }
     }
-  }
-}
+};
 </script>
 <style scoped>
 </style>
